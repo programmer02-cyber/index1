2bb8bbc226baaa35b0266492572dcb998676e4e2
xurshidbek_dastur <xmominov18@gmail.com>
2023-08-09 14:47:12 +0500

    nyu js

---
+++ index.js/index.js
@@ -0,0 +1,34 @@
+// console.log(12);
+// console.log(12);
+// console.log('hello');
+
+
+
+// let son = 14
+// son = 20
+
+// var son1 = 20
+// son1 = 15
+
+// const son2 = 30
+// son2 = 33
+
+
+// console.log(son1); 
+
+// var
+// const
+
+
+// data types 
+let str = "salom"   //string data types//
+let num = 12 //number data types//
+let bool = true //false//boolean data types//
+let und = undefined // undefined//
+let nul = null //null data types//
+
+console.log(typeof bool);
+
+// let s = 10
+// let c ='20'
+// console.log(s,c);
